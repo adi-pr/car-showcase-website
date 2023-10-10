273d5512a47dfdd132a2edf1602b9e9d16614fe4
Ruben <adityaprasad7123@gmail.com>
2023-10-10 13:19:07 -0400

    Made function to calculate rent price

--- utils/index.ts
+++ utils/index.ts
@@ -12,3 +12,18 @@ export async function fetchCars() {
 
   return results;
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; 
+  const mileageFactor = 0.1; 
+  const ageFactor = 0.05; 
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
