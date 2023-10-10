d74fca82a476c20c8e8fc274c8baa0d803b8ee7d
Ruben <adityaprasad7123@gmail.com>
2023-10-10 14:25:50 -0400

    Refactored Custom Button

--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,9 @@ export interface CustomButtonProps {
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps {
@@ -13,7 +16,7 @@ export interface SearchManufacturerProps {
 }
 
 export interface CarProps {
-  city_mpg: number; 
+  city_mpg: number;
   class: string;
   combination_mpg: number;
   cylinders: number;
