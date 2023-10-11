92e46ace8cfb1f21af370e455323b0011250243a
Ruben <adityaprasad7123@gmail.com>
2023-10-11 11:52:30 -0400

    Added Filter functionality

--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,7 @@
 import { CarProps, FiltersProps } from "@/types";
 
 export async function fetchCars(filters: FiltersProps) {
-  const { manufacturer, year, fuel, limit, model} = filters;
-
+  const { manufacturer, year, fuel, limit, model } = filters;
 
   const headers = {
     "X-RapidAPI-Key": "5a7b518514msh283ed423ef3df24p1751a1jsn32e28c552a3e",
@@ -39,14 +38,24 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getImage");
 
-  const {make, model, year} = car
+  const { make, model, year } = car;
 
-  url.searchParams.append('customer', 'hrjavascript-mastery');
-  url.searchParams.append('make', make);
-  url.searchParams.append('modelFamily', model.split(' ')[0]);
-  url.searchParams.append('zoomType', 'fullscreen');
-  url.searchParams.append('modelYear', `${year}`);
-  url.searchParams.append('angle', `${angle}`);
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
 
-  return `${url}`
+  return `${url}`;
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathname;
 };
